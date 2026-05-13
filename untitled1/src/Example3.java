867702bc2c1e9d83be7f1f642276cba66b019501
ankkithapa <ankithapamagar@gmail.com>
2026-05-13 14:44:39 +0900

    Add files via upload

---
+++ untitled1/src/Example3.java
@@ -0,0 +1,11 @@
+import java.util.*;
+
+public class Example3 {
+    public static void main(String[] args) {
+        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5);
+
+        for (int number : list) {
+            System.out.println(number);
+        }
+    }
+}
